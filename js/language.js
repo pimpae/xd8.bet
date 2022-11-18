54105cd00e309a36cac1cd85162d23752099d447
pimpae <pimpae@hotmail.com>
2022-11-18 20:39:04 +0700

    21:22 16-Nov-22

---
+++ js/language.js
@@ -0,0 +1,15 @@
+function lang_th() {
+    $('.lang1').text("ออนไลน์ สล็อต");   
+    $('.lang2').text("ไลฟ์ คาสิโน");   
+    $('.lang3').text("เกมโต๊ะ");   
+    $('.lang4').text("กีฬา");   
+    $('.lang5').text("โปรโมชั่น");   
+}
+
+function lang_en() { 
+    $('.lang1').text("ONLINE SLOTS");   
+    $('.lang2').text("LIVE CASINO");   
+    $('.lang3').text("TABLE GAMES");   
+    $('.lang4').text("SPORT");   
+    $('.lang5').text("PROMOTIONS");   
+}
