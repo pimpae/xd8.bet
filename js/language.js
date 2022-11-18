39f787ff75ba61796f366d4f62dce3e3b616e20c
pimpae <pimpae@hotmail.com>
2022-11-18 22:10:12 +0700

    21:22 16-Nov-22

--- js/language.js
+++ js/language.js
@@ -3,7 +3,17 @@ function lang_th() {
     $('.lang2').text("ไลฟ์ คาสิโน");   
     $('.lang3').text("เกมโต๊ะ");   
     $('.lang4').text("กีฬา");   
-    $('.lang5').text("โปรโมชั่น");   
+    $('.lang5').text("โปรโมชั่น");     
+    $('.lang6').text("ล็อคอิน");   
+    $('.lang7').text("สมัครสมาชิก");  
+    $('.lang8').text("เกมโชว์");   
+    $('.lang9').text("สล็อตยอดนิยม");  
+    $('.lang10').text("ผู้ให้บริการเกม");   
+    $('.lang11').text("การสนับสนุนทางออนไลน์");  
+    $('.lang12').text("ข้อกำหนดและเงื่อนไข");  
+    $('.lang13').text("เกี่ยวกับเรา");  
+    $('.lang14').text("เว็บเกมออนไลน์ที่ดีที่สุดในเอเชีย");  
+    $('.lang15').text("XD8.BET สล็อตออนไลน์ คาสิโนสด พนันบอล บอลสเต็ป <br> มากกว่า 60 แมทช์ต่อสัปดาห์");  
 }
 
 function lang_en() { 
@@ -12,4 +22,14 @@ function lang_en() {
     $('.lang3').text("TABLE GAMES");   
     $('.lang4').text("SPORT");   
     $('.lang5').text("PROMOTIONS");   
+    $('.lang6').text("SIGN IN");   
+    $('.lang7').text("JOIN NOW");   
+    $('.lang8').text("GAMES SHOWS");   
+    $('.lang9').text("POPULAR SLOTS");   
+    $('.lang10').text("GAME PROVIDERS");   
+    $('.lang11').text("Online Support");  
+    $('.lang12').text("Terms and Conditions");  
+    $('.lang13').text("About Us");  
+    $('.lang14').text("THE BEST ONLINE GAMBLING SITE IN ASIA");  
+    $('.lang15').text("XD8.BET Online Slots, Live Casinos, Football Betting, Football Steps, more than 60 matches per week");  
 }
